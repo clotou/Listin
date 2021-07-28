eabee13c81fdd94af830ab425ec1691d3610c030
clotilde lacaille <lacailleclotilde@gmail.com>
2021-07-28 14:03:11 +0200

    delete list ok

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,7 +2,7 @@ class List < ApplicationRecord
   belongs_to :user
   has_many :items, dependent: :destroy
   has_many :invites
-  has_many :users, through: :invites
+  has_many :users, through: :invites, dependent: :destroy
 
   validates :name, presence: true, uniqueness: true
 end
